cc3516a86df9f96abb8a12bfe39cfd9e40141066
Cijid <mmkcijid@gmail.com>
2026-07-29 13:47:24 -0600

    Started building minesweeper

---
+++ minesweepah/tests/App.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../src/App";
+
+
+//Act - User Actions
+//Assert - Validate application input/output interactions
+
+describe("App Functionality", () => {
+  beforeEach(() => {
+    //Arrange
+    render(<App/>);
+  });
+
+  it("renders the Application", () => {
+    //Act
+    //  No Act steps needed
+    // Assert
+    //  Check if the Minesweeper header renders on screen
+    expect (screen.getByText("Minesweeper")).toBeInTheDocument();
+  });
+
+
+
+
+
+
+
+});
